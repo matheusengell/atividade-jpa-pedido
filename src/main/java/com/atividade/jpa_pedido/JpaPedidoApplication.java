70eb6f7da099bc2a67159671318573176b82cf97
matheus engel <matheusengelfl@gmail.com>
2026-03-20 13:48:04 -0300

    inicia a atividade

---
+++ src/main/java/com/atividade/jpa_pedido/JpaPedidoApplication.java
@@ -0,0 +1,13 @@
+package com.atividade.jpa_pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atividade/jpa_pedido/JpaPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividade.jpa_pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
